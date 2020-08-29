f69f4663955d451fe248323934c29156a231b462
Ariful4593 <arifkhagoria2015@gmail.com>
2020-08-30 00:35:24 +0600

    My first e-commerce website

---
+++ main.js
@@ -0,0 +1,17 @@
+class myclas{
+    myfun1(){
+        console.log("Hello dear");
+    }
+    myfun2(){
+        console.log("Hello dear");
+    }
+    myfun3(){
+        console.log("Hello dear");
+    }
+    myfun4(){
+        console.log("Hello dear");
+    }
+}
+var obj = new myclas;
+
+obj.myfun1();
